e665aa3436c90fd68f5f4c7a0e19cf1c29ae6990
omagdiel <omagdiel@gmail.com>
2018-02-21 08:53:25 -0700

    updated seeds and user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-@user = User.create!(email: "test@test.com", first_name: "Test", last_name: "User", password:"asdfasdf", password_confirmation:"asdfasdf")
+@user = User.create!(email: "test@test.com",
+                    first_name: "Test",
+                    last_name: "User",
+                    password:"asdfasdf",
+                    password_confirmation:"asdfasdf",
+                    roles: "user"
+                    )
 puts "1 User created"
 
 10.times do |post|
-  Post.create!(title: "My Post #{post}", body: "This is some great text information and I am going to show it all to you", status: 0, user_id: @user.id)
+  Post.create!(title: "My Post #{post}",
+              body: "This is some great text information and I am going to show it all to you",
+              status: 0,
+              user_id: @user.id
+              )
 end
 puts "10 posts created"
