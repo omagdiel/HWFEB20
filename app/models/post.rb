14132907441de802d99e406cb9be6fc681fb2e15
omagdiel <omagdiel@gmail.com>
2018-02-21 07:39:33 -0700

    created status for post and made user post connections

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+  enum status:{ draft: 0, published: 1}
   validates_presence_of :title, :body
   belongs_to :user
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,7 @@
+@user = User.create!(email: "test@test.com", first_name: "Test", last_name: "User", password:"asdfasdf", password_confirmation:"asdfasdf")
+puts "1 User created"
+
 10.times do |post|
-  Post.create!(title: "My Post #{post}", body: "This is some great text information and I am going to show it all to you")
+  Post.create!(title: "My Post #{post}", body: "This is some great text information and I am going to show it all to you", status: 0, user_id: @user.id)
 end
 puts "10 posts created"
