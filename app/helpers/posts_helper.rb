4225fc0d938b8f49ac0acdb31ea67fd5e36d63bd
omagdiel <omagdiel@gmail.com>
2018-02-23 18:09:34 -0700

    finished carrierwave installation

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,17 @@
 module PostsHelper
+
+  def image_generator(height:, width:)
+    "http://placehold.it/#{height}x#{width}"
+  end
+
+  def post_img img, type
+
+    if img.model.image?
+      img
+    elsif type == 'main'
+      image_generator(height: '350', width: '200')
+    else
+      image_generator(height: '600', width: '400')
+    end
+  end
 end
