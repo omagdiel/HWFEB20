f2e63179f92779dfaf207737aec9e2175f14e77b
omagdiel <omagdiel@gmail.com>
2018-02-26 09:35:08 -0700

    fixed carrierwave implementation to render images on page.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -4,14 +4,4 @@ module PostsHelper
     "http://placehold.it/#{height}x#{width}"
   end
 
-  def post_img img, type
-
-    if img.model.image?
-      img
-    elsif type == 'main'
-      image_generator(height: '350', width: '200')
-    else
-      image_generator(height: '600', width: '400')
-    end
-  end
 end
