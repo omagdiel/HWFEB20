c8528008ae797c9777454b9b7fb05e75d5ce0b18
omagdiel <omagdiel@gmail.com>
2018-02-27 07:45:34 -0700

    Implemented css styles for index pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,13 +3,16 @@ module ApplicationHelper
     "active" if current_page?(path)
   end
 
-  def login_helper style = ''
+  def login_helper
+    # style
     if !logged_in?(:user) || !logged_in?(:admin)
-      (link_to "Register", new_user_registration_path, class: style) +
+
+      (link_to "Register",new_user_registration_path) +
       " ".html_safe +
-      (link_to "Login", new_user_session_path, class: style)
+      (link_to "Login", new_user_session_path)
     else
-      link_to "Logout", destroy_user_session_path, method: :delete, class: style
+
+      link_to "Logout", destroy_user_session_path, method: :delete
     end
   end
 
@@ -34,9 +37,10 @@ module ApplicationHelper
     nav_links = ''
 
     nav_items.each do |item|
-      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}> " 
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style}'>#{item[:title]}</a></#{tag_type}>"
     end
 
     nav_links.html_safe
   end
 end
+# class='#{style} #{active? item[:url]}
